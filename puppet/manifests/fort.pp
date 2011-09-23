f104e5e70dd73ddb798f715ee418c5aa9b213489
Blaine Cook <romeda@gmail.com>
2011-09-23 16:57:44 +0100

    move restpose to a module, enable service.

--- puppet/manifests/fort.pp
+++ puppet/manifests/fort.pp
@@ -35,7 +35,7 @@ include pip
 include postgres
 include project
 include apache
-
+include restpose
 
 if $vagrant {
     if $onafort {
@@ -242,66 +242,3 @@ file { "/usr/local/bin/pipe_runner":
     mode    => "0755",
 }
 
-
-# restpose
-
-package { ["libxapian-dev", "uuid-dev", "libgcrypt11", "libgcrypt11-dev", "autoconf", "libtool", "texinfo"]:
-  ensure => present
-}
-
-$restpose = "rboulton-restpose-117a097"
-
-exec { "prep-restpose":
-   command => "/bin/rm -rf /tmp/${restpose}",
-   cwd => "/tmp",
-   before => Exec["fetch-restpose"],
-   unless => "/usr/bin/test -f /usr/local/bin/restpose"
-}
-
-exec { "fetch-restpose":
-   require => Package["curl"],
-   command => "/usr/bin/curl -o ${restpose}.tar.gz https://nodeload.github.com/rboulton/restpose/tarball/r0.7.2",
-   cwd => "/tmp",
-   before => Exec["extract-restpose"],
-   unless => "/usr/bin/test -f /usr/local/bin/restpose"
-}
-
-exec { "extract-restpose":
-    command => "/bin/tar xzf ${restpose}.tar.gz",
-    cwd => "/tmp",
-    before => Exec["bootstrap-restpose"],
-    unless => "/usr/bin/test -f /usr/local/bin/restpose"
-}
-
-file { "/tmp/${restpose}":
-    require => Exec["extract-restpose"]
-}
-
-file { "/tmp/${restpose}/bootstrap": }
-
-exec { "bootstrap-restpose":
-    require => [Exec["extract-restpose"], Package["autoconf", "libtool"], File["/tmp/${restpose}/bootstrap"]],
-    command => "/tmp/${restpose}/bootstrap",
-    cwd => "/tmp/${restpose}",
-    creates => "/tmp/${restpose}/configure",
-    before => Exec["configure-restpose"]
-}
-
-file { "/tmp/${restpose}/configure": }
-
-exec { "configure-restpose":
-    require => [File["/tmp/${restpose}", "/tmp/${restpose}/configure"], Package["libxapian-dev", "uuid-dev", "libgcrypt11", "libgcrypt11-dev"]],
-    command => "/tmp/${restpose}/configure --enable-static=yes",
-    cwd => "/tmp/${restpose}",
-    creates => "/tmp/${restpose}/Makefile",
-    before => Exec["build-restpose"]
-}
-
-file { "/tmp/${restpose}/Makefile": }
-
-exec { "build-restpose":
-    require => [File["/tmp/${restpose}", "/tmp/${restpose}/Makefile"], Package["texinfo"]],
-    command => "/usr/bin/make install",
-    cwd => "/tmp/${restpose}",
-    creates => "/usr/local/bin/restpose"
-}
